c5ce85c168e2206885bb12d351d57376720fdfa0
jolth <jolthgs@gmail.com>
2017-05-02 21:48:04 -0500

    connect to database

--- sendsuspended.py
+++ sendsuspended.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # Copyright (c) 2017 Jorge Toro.
+# $ python sendsuspended.py -u postgres -d rastree -W qwerty
 """send sms to the your suspended"""
 from __future__ import print_function
 import sys
 import csv
 import argparse
+import psycopg2
 
 parser = argparse.ArgumentParser(
         description="Location the to vehicles suspended"
     )
 parser.add_argument("-d", action="store", dest="dbname")
+parser.add_argument("-u", action="store", dest="user")
+parser.add_argument("-W", action="store", dest="password")
 parser.add_argument("-c", action="store", dest="csvfile")
-
 arg = parser.parse_args()
 
 
@@ -30,6 +33,13 @@ def db_reader(cursor):
 
 
 if __name__ == "__main__":
+    #connect db
+    conn = psycopg2.connect("dbname={0} user={1}\
+            password={2}".format(arg.dbname, arg.user, arg.password))
+    raise SystemExit
+
+    reader = db_reader()
+    reader.__next__()
     if arg.csvfile:
         for row in csv_reader(arg.csvfile):
-            print(row['plate'])
+            reader.send(row['plate'])
