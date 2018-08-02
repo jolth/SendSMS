964310e82fb58d87c1a894f3207b2f02ea0b9beb
jolth <jolthgs@gmail.com>
2018-08-02 11:27:33 -0500

    support for more of a phone number

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2016 Jorge Toro.
 """file csv parsing:
-usage: create_spreadsheet template file output_file 
+usage: create_spreadsheet template file output_file
 
-example: ./create_spreadsheet.py "\${name}, happy birthday" file.csv out.csv 
+example: ./create_spreadsheet.py "\${name}, happy birthday" file.csv out.csv
 """
-from __future__ import print_function
+# from __future__ import print_function
 import sys
 import csv
 from string import Template
 
-#csv.register_dialect("comcel", delimiter='|')
 
+# csv.register_dialect("comcel", delimiter='|')
 def reader(f):
     """reader file csv.
     example: r = reader(file.csv); r.__next__()"""
@@ -24,24 +24,32 @@ def reader(f):
 def temparsing(buffer, template):
     """template parsing"""
     print("Template [length: {1}]: {0}".format(template.template,
-        len(template.template))) # debug
+          len(template.template)))  # debug
     try:
         for row in buffer:
-            yield (row, template.substitute(row, 
-                name=row['name'].split(' ')[0]))
+            yield (row, template.substitute(row,
+                   name=row['name'].split(' ')[0]))
     except KeyError as e:
         print("{0} key not existing into .csv".format(e))
 
 
 def comcelwriter(f, buffer):
     with open(f, r'wt') as f:
-        for k, d in buffer: 
-            f.write("{0[phone]}|{1}\n".format(k, d))
+        for k, d in buffer:
+            phone = k['phone']
+            if phone.find('|') != -1:
+                phones = phone.split('|')
+                for p in phones:
+                    f.write("{0}|{1}\n".format(p, d))
+            else:
+                # f.write("{0[phone]}|{1}\n".format(k, d))
+                f.write("{0}|{1}\n".format(phone, d))
+                # print(phone)
 
 
 if __name__ == "__main__":
-    template = Template(sys.argv[1]) 
+    template = Template(sys.argv[1])
     reads = reader(sys.argv[2])
-    
+
     b = temparsing(reads, template)
     comcelwriter(sys.argv[3], b)
